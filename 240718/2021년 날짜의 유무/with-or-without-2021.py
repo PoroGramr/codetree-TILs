1eac67b9c4884ad4099a6da205e37d51cf1c8ebb
JUNSEO PARK <pjs9177@naver.com>
2024-07-18 17:10:16 +0900

    Title: 2021년 날짜의 유무, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240718/2021년 날짜의 유무/with-or-without-2021.py
@@ -0,0 +1,12 @@
+days = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+def checkDay(a,b):
+    if days[a-1] >= b:
+        return "Yes"
+    else:
+        return "No"
+
+a,b = map(int,input().split())
+
+answer = checkDay(a,b)
+print(answer)
