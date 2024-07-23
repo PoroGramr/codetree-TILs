41cf65d2047060345086ae855d053b64e44a191f
JUNSEO PARK <pjs9177@naver.com>
2024-07-23 20:33:13 +0900

    Title: 2개 이상의 알파벳, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240723/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 240723/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -1,16 +1,13 @@
-def chcek(data):
-    data = list(data)
-    count = []
-    for i in data:
-        if i in count:
-            print("Yes")
-            return
-        else:
-            count.append(i)
+def check_unique_alphabets(A):
+    # 문자열 A에서 서로 다른 알파벳의 집합을 구합니다.
+    unique_alphabets = set(A)
+    
+    # 집합의 크기가 2개 이상이면 Yes, 아니면 No를 출력합니다.
+    if len(unique_alphabets) >= 2:
+        return "Yes"
+    else:
+        return "No"
 
-    print("No")
-    return 
-
-
-data  = input()
-chcek(data)
+# 사용자로부터 문자열 입력을 받습니다.
+inputString = input()
+print(check_unique_alphabets(inputString))
