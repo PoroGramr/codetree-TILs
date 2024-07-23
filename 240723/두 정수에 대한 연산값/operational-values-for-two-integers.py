2ce12459cfa7adf2d05a360dec9296cda7cefc99
JUNSEO PARK <pjs9177@naver.com>
2024-07-23 19:24:13 +0900

    Title: 두 정수에 대한 연산값, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240723/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 240723/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -1,11 +1,12 @@
 def check(a,b):
-    data = [a,b]
-    data.sort()
-    data[0] = data[0] * 2
-    data[1] = data[1] + 25
+    if a > b:
+        print(a + 25, end=" ")
+        print(b * 2)
+    else:
+        print(a * 2, end=" ")
+        print(b + 25)
+        
 
-    print(data[0], end=" ")
-    print(data[1])
-
+        
 a,b = map(int,input().split())
 check(a,b)
