9b20c78a8913ecdd64780c47c9588491fe1ffbf5
JUNSEO PARK <pjs9177@naver.com>
2024-08-02 12:48:58 +0900

    Title: k번째로 신기한 문자열, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240802/k번째로 신기한 문자열/kth-special-string.py
+++ 240802/k번째로 신기한 문자열/kth-special-string.py
@@ -2,9 +2,9 @@ n, k , start = input().split()
 data = []
 for _ in range(int(n)):
     data.append(input())
-
+data.sort()
 answerData = []
 for _data in data:
     if _data[0:len(start)] == start:
         answerData.append(_data)
-print(answerData)
+print(answerData[int(k) - 1])
