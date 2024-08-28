03dfff97bb6f3914e198e09c169138eb4c5facf5
JUNSEO PARK <pjs9177@naver.com>
2024-08-28 22:16:51 +0900

    Title: DateTime to DateTime, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240828/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,21 @@
+def check(a,b,c):
+    if a < 11:
+        return "-1"
+    if a == 11 and b <= 11 and c <= 11:
+        return "-1"
+    
+    dayMin = a * 24 * 60
+    hourMin = b * 60
+    totalMin = dayMin + hourmin + c
+
+    pinMin = (11 * 24 * 60) + (11 * 60) + 11
+
+    answer = totalMin - pinMin
+    
+    return answer
+
+
+
+
+a, b, c = map(int, input().split())
+print(check(a,b,c))
