a107190d4364521c15f1f9b5a91609f43aa8d889
JUNSEO PARK <pjs9177@naver.com>
2024-08-28 16:49:06 +0900

    Title: 원점으로부터의 거리, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240828/원점으로부터의 거리/distance-from-origin.py
+++ 240828/원점으로부터의 거리/distance-from-origin.py
@@ -13,7 +13,9 @@ for i in range(n):
     x, y = map(int, input().split())
     xys.append(xy(x,y, i+1))
 
+# 멘하턴 거리의 맞게 정렬, 거리가 같다면 번호가 작은 점부터 출력
 xys.sort(key = lambda x: (abs((0 - x.x)) + abs((0 - x.y)), x.num))
 
+# 요구사항에 맞게 출력
 for s in xys:
     print(s.num)
