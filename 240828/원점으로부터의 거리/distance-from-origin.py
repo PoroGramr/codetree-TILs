71a86b2b24c98c21d32b5ce568e3479f8556ca5d
JUNSEO PARK <pjs9177@naver.com>
2024-08-28 16:47:10 +0900

    Title: 원점으로부터의 거리, Time: 130ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240828/원점으로부터의 거리/distance-from-origin.py
+++ 240828/원점으로부터의 거리/distance-from-origin.py
@@ -13,7 +13,7 @@ for i in range(n):
     x, y = map(int, input().split())
     xys.append(xy(x,y, i+1))
 
-xys.sort(key = lambda x: (abs((0 - x.x) + (0 - x.y)), x.num))
+xys.sort(key = lambda x: (abs((0 - x.x) + (0 - x.y)), -x.num))
 
 for s in xys:
     print(s.num)
