3317641892d133502dd5f3ef1cd957335cc72cc8
JUNSEO PARK <pjs9177@naver.com>
2024-08-29 23:11:11 +0900

    Title: 왔다 갔던 구역 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240829/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240829/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,31 +1,26 @@
-# 이동 기록 리스트, 왼쪽과 오른쪽 넉넉하게 확보
-data = [0] * 1000
+data = [0] * 100
 
-# 초기 위치 설정 (중간을 0으로 보기 위해 500으로 설정)
 n = int(input())
-currentX = 500
-
-# 명령에 따른 이동
+currentX = 49
 for _ in range(n):
-    x, direction = input().split()
+    x, direction = map(str, input().split())
     x = int(x)
-    
+
     if direction == "R":
-        # 오른쪽으로 이동하는 경우, 끝점은 포함하지 않음
+        # 오른쪽으로 이동하는 경우
         for i in range(currentX, currentX + x):
             data[i] += 1
         currentX += x  # 현재 위치 업데이트
         
     elif direction == "L":
-        # 왼쪽으로 이동하는 경우, 끝점은 포함하지 않음
-        for i in range(currentX, currentX - x, -1):
+        # 왼쪽으로 이동하는 경우
+        for i in range(currentX - 1, currentX - x - 1, -1):
             data[i] += 1
         currentX -= x  # 현재 위치 업데이트
 
-# 2번 이상 지나간 구간 계산
 answer = 0
-for count in data:
-    if count >= 2:
+for i in data:
+    if i >= 2:
         answer += 1
 
 print(answer)
