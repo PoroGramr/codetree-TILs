042d43c37144e8c7509e868b2da72b737928f974
JUNSEO PARK <pjs9177@naver.com>
2024-09-04 21:59:52 +0900

    Title: 모이자, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240904/모이자/gather.py
+++ 240904/모이자/gather.py
@@ -4,17 +4,22 @@ n = int(input())
 
 homeData = list(map(int, input().split()))
 
+# 최소값 초기값을 세팅
 minValue = -sys.maxsize
 
 for i in range(n):
     moveSum = 0
+
+    # 이동거리 계산
     for k in range(n):
         moveSum += abs((i - k)) * homeData[k]
 
+    # 첫번째 루프에서는 최소값을 초기화해줌
     if i == 0:
         minValue = moveSum
         continue
 
+    # 그 이후부터 최소값을 탐색
     if moveSum < minValue:
         minValue = moveSum
 
