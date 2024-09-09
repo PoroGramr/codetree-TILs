d49eb20c3503956e71712522cc04ac638afa7764
JUNSEO PARK <pjs9177@naver.com>
2024-09-09 15:39:08 +0900

    Title: 이상한 진수 2, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240909/이상한 진수 2/awkward-digits-2.py
+++ 240909/이상한 진수 2/awkward-digits-2.py
@@ -5,12 +5,22 @@ numList = list(num)
 # 첫번쨰 for문을 거치는지 안거치는 지 확인을 위한 변수
 changeCount  = 0
 
-# 만약 0이 있다면 1로 바꾸고 for문 탈출
-for i in range(len(numList)):
-    if numList[i] == '0':
-        numList[i] = '1'
+# 문자열의 길이가 1일 경우
+if len(numList) == 1:
+    if numList[0] == '0':
+        numList[0] = '1'
         changeCount = 1
-        break
+    else:
+        numList[0] = '0'
+        changeCount = 1
+
+# 만약 0이 있다면 1로 바꾸고 for문 탈출
+if changeCount == 0:
+    for i in range(len(numList)):
+        if numList[i] == '0':
+            numList[i] = '1'
+            changeCount = 1
+            break
 
 # 0이 없다면 1을 0으로 바꾸고 for문 탈출
 # 위 for문을 통과하고 변화가 없다면 실행됨
