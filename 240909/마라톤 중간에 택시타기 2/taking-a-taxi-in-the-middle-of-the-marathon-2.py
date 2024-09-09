1e28a715dd807216e38708fffa1adc2a501c09b2
JUNSEO PARK <pjs9177@naver.com>
2024-09-09 15:57:11 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240909/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,46 @@
+"""
+- 문제 이해
+    - 마라톤은 총 N개의 체크포인트로 구성, 
+    1번부터 순서대로 체크포인트를 거친 후 N번 체크포인트까지 도달해야 완주
+
+    - 개발자 A는 체크포인트 한개만 건너뛰고 완주하려함
+    - 이때 개발자A가 움직이는 최소거리를 구하여야 함
+    - 이동거리 계산은 두 지점 간의 거리를 택시 거리로 계산
+
+- 아이디어
+    - 처음시작 지점(1번 체크포안트)과 마지막 N번째 체크포인트는 무조건 거쳐야 함
+    - 2번 체크포인트 부터 N-1번쨰 체크포인트까지 에서 한번만 스킵할 수 있음
+    - for문을 통해 한 체크포인트를 뺴고 총 거리를 계산하며 최소값을 업데이트해보자
+"""
+import sys
+
+n = int(input())
+
+xyData = []
+
+for i in range(n):
+    x,y = map(int, input().split())
+    xyData.append([x, y])
+
+# [[0, 0], [8, 3], [11, -1], [10, 0]]
+
+minLen = sys.maxsize
+
+# 첫번째 지점과 마지막 지점 제외
+for k in range(1, n-1):
+    currentMin = 0
+    for j in range(1, n):
+
+        # 상단 for문에서 선택된 N번째 체크포인트는 스킵 
+        if j == k:
+            continue
+        
+        if j-1 == k:
+            currentMin += abs(xyData[j-2][0] - xyData[j][0]) + abs(xyData[j-2][1] - xyData[j][1])
+        
+        else:
+            currentMin += abs(xyData[j-1][0] - xyData[j][0]) + abs(xyData[j-1][1] - xyData[j][1])
+
+    minLen = min(minLen, currentMin)
+    
+print(minLen)
