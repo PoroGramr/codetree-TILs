99472e23ded084b7575f529542b8c901283fffa1
JUNSEO PARK <pjs9177@naver.com>
2024-09-09 15:21:17 +0900

    Title: 체크판위에서 2, Time: 114ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240909/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,20 @@
+R, C = map(int, input().split()) # 가로 세로 길이
+
+data = []
+
+for _ in range(R):
+    data.append(list(map(str, input().split())))
+
+answer = 0
+
+for i in range(1,R): # 0,0은 이미 밟고 시작하기에 1부터 시작
+    for k in range(1, C):
+        for j in range(i + 1, R -1):
+            for a in range(k + 1, C - 1):
+                 if data[0][0] != data[i][k] and \
+                   data[i][k] != data[j][a] and \
+                   data[j][a] != data[R - 1][C - 1]:
+                   answer += 1
+
+
+print(answer)
