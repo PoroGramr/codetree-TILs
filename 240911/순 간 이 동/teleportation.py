afae3ae82fca5e1c9ed4ba89b6a82785f56cdc00
JUNSEO PARK <pjs9177@naver.com>
2024-09-11 21:32:04 +0900

    Title: 순 간 이 동, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240911/순 간 이 동/teleportation.py
+++ 240911/순 간 이 동/teleportation.py
@@ -3,13 +3,13 @@ import sys
 A, B, x,y = map(int, input().split())
 
 # 바로 가는 경우
-case1 = B - A
+case1 = abs(B - A)
 
 # x를 들렸다가 가는 경우
 case2 = abs(A - x) + abs(B - y)
 
 # y를 들렸다가 가는 경우
-case3 = abs(y - A) +abs(B - x)
+case3 = abs(y - A) + abs(B - x)
 
 answer = min(case1, case2, case3)
 
