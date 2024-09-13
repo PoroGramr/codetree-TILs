ab8d93de87acb26d5a8f88626f1b6d211b05719c
JUNSEO PARK <pjs9177@naver.com>
2024-09-13 22:32:49 +0900

    Title: 두 번째로 작은 수의 위치, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240913/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
+++ 240913/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
@@ -5,9 +5,13 @@ arr = list(map(int, input().split()))  # 주어진 n개의 수 리스트
 unique_sorted = sorted(set(arr))
 
 # 두 번째로 작은 수가 존재하는지 확인
-if len(unique_sorted) <= 2:
+if len(unique_sorted) < 2:
     print(-1)
 else:
     second_min = unique_sorted[1]  # 두 번째로 작은 수
-    # 해당 수의 위치를 출력 (1부터 시작하므로 +1)
-    print(arr.index(second_min) + 1)
+    # 두 번째로 작은 수가 여러 번 등장하면 -1을 출력
+    if arr.count(second_min) > 1:
+        print(-1)
+    else:
+        # 해당 수의 위치를 출력 (1부터 시작하므로 +1)
+        print(arr.index(second_min) + 1)
