55d7109514d8a546bf7f37a6396a5e4142827313
JUNSEO PARK <pjs9177@naver.com>
2024-10-01 20:14:45 +0900

    Title: 진수 to 진수, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 241001/진수 to 진수/transformation-of-number-system.py
+++ 241001/진수 to 진수/transformation-of-number-system.py
@@ -11,11 +11,15 @@ for k in range(len(data)):
         num += int(data[k])
     else:
         num += (a ** k) * int(data[k])
+# 10진수로 변환한 n을 b진수로 변환
 answerNum = []
 while num != 0:
     answerNum.append(str(num % b))
     num //= b
+# 역순으로 저장되어있기에 리버스 해준다
 answerNum.reverse()
+
+# 리스트를 문자열로 변환
 answer =""
 for a in answerNum:
     answer = answer + str(a)
