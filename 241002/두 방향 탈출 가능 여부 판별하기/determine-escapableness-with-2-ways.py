b978716280db6bc1582c9f38534ecf9e3bdab63b
JUNSEO PARK <pjs9177@naver.com>
2024-10-02 18:12:55 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 127ms, Memory: 26MB, Status: Passed - CodeTree

--- 241002/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 241002/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,20 +1,26 @@
 def dfs(y, x, M, N):
+    # 목적지에 도착한 경우
     if y == (M - 1) and x == (N - 1):
-        return 1
+        return True
 
+    # 오른쪽, 아래 방향으로만 이동
     for py, px in ((0, 1), (1, 0)):
         ny, nx = y + py, x + px
+        # 이동 가능한 범위 내, 이동 가능한 칸(1), 아직 방문하지 않은 경우
         if 0 <= ny < M and 0 <= nx < N and data[ny][nx] == 1 and v[ny][nx] == 0:
-            v[ny][nx] = 1
-            if dfs(ny, nx, M, N) == 1:  # 탈출 경로를 찾으면 1 반환
-                return 1
-            v[ny][nx] = 0  # 경로를 찾지 못한 경우 방문 처리 해제
+            v[ny][nx] = 1  # 방문 처리
+            if dfs(ny, nx, M, N):  # 경로를 찾으면 즉시 True 반환
+                return True
 
-    return 0
+    return False
 
 N, M = map(int, input().split())
 data = [list(map(int, input().split())) for _ in range(N)]
 v = [[0 for _ in range(M)] for _ in range(N)]
 v[0][0] = 1  # 시작 지점을 방문 처리
-answer = dfs(0, 0, N, M)
-print(answer)
+
+# 탈출 가능한 경로가 있으면 1, 없으면 0 출력
+if dfs(0, 0, N, M):
+    print(1)
+else:
+    print(0)
