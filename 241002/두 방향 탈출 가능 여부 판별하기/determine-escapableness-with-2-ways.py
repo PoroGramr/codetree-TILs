3b8dae3a1c2605ae1e638681a3a019379e9dd269
JUNSEO PARK <pjs9177@naver.com>
2024-10-02 18:09:51 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 5009ms, Memory: 38MB, Status: Time Limit Exceed - CodeTree

--- 241002/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 241002/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,19 +1,20 @@
-def dfs(y,x,M,N):
-
+def dfs(y, x, M, N):
     if y == (M - 1) and x == (N - 1):
         return 1
 
-    for py,px in ((0,1),(1,0)):
-        ny ,nx = y + py, x + px
-        if 0 <= ny < M and 0 <= nx <N and v[ny][nx] == 0:
+    for py, px in ((0, 1), (1, 0)):
+        ny, nx = y + py, x + px
+        if 0 <= ny < M and 0 <= nx < N and data[ny][nx] == 1 and v[ny][nx] == 0:
             v[ny][nx] = 1
             if dfs(ny, nx, M, N) == 1:  # 탈출 경로를 찾으면 1 반환
                 return 1
+            v[ny][nx] = 0  # 경로를 찾지 못한 경우 방문 처리 해제
+
     return 0
 
-
-N,M = map(int, input().split())
-data = [list(map(int, input().split())) for _ in range(M)]
-v = [[0 for _ in range(N)] for _ in range(M)]
-answer = dfs(0,0,N,M)
+N, M = map(int, input().split())
+data = [list(map(int, input().split())) for _ in range(N)]
+v = [[0 for _ in range(M)] for _ in range(N)]
+v[0][0] = 1  # 시작 지점을 방문 처리
+answer = dfs(0, 0, N, M)
 print(answer)
