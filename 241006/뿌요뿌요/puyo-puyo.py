327d3278094aada76bf975028f567b1e9a58c048
JUNSEO PARK <pjs9177@naver.com>
2024-10-06 02:52:52 +0900

    Title: 뿌요뿌요, Time: 133ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241006/뿌요뿌요/puyo-puyo.py
@@ -0,0 +1,49 @@
+def dfsMax(y,x,i):
+    v[y][x] = 1
+    count = 1
+
+    for py, px in ((0,1),(0,-1),(1,0),(-1,0)):
+        ny, nx = y + py, x + px
+        if 0 <= ny < N and 0 <= nx < N and data[ny][nx] == i and v[ny][nx] == 0:
+            count += dfsMax(ny,nx,i)
+
+    return count
+
+def dfsBomb(y,x,i):
+    v[y][x] = 1
+    count = 1
+    for py, px in ((0,1),(0,-1),(1,0),(-1,0)):
+        ny, nx = y + py, x + px
+        if 0 <= ny < N and 0 <= nx < N and data[ny][nx] == i and v[ny][nx] == 0:
+            count += dfsBomb(ny,nx,i)
+    return count
+    
+
+    
+
+
+
+
+N = int(input())
+
+data = [list(map(int,input().split())) for _ in range(N)]
+
+big = 0
+# 최대 블럭의 크기
+for i in range(1,100):
+    v = [[0 for _ in range(N)] for _ in range(N)]
+    for y in range(N):
+        for x in range(N):
+            tmp = dfsMax(y,x,i)
+            big = max(big,tmp)
+
+bombCount = 0
+# 터지게 될 블럭의 숫자
+for i in range(1,100):
+    v = [[0 for _ in range(N)] for _ in range(N)]
+    for y in range(N):
+        for x in range(N):
+            bomb = dfsBomb(y,x,i)
+            if 4 <= bomb:
+                bombCount += 1
+print(bombCount, big)
