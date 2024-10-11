b9402de4e860de0dcd39db7cf04b52b4d213f5ab
JUNSEO PARK <pjs9177@naver.com>
2024-10-11 17:23:59 +0900

    Title: 두 수의 합, Time: 155ms, Memory: 36MB, Status: Passed - CodeTree

--- 241011/두 수의 합/sum-of-two-num.py
+++ 241011/두 수의 합/sum-of-two-num.py
@@ -1,26 +1,18 @@
-import sys
-
-# 재귀 깊이 제한 해제 또는 증가 (예: 10,000으로 설정)
-sys.setrecursionlimit(100000)
-
-
-def dfs(i, res):
-    global ans
-
-    if i == n:
-        if res == k:
-            ans += 1
-        return
-    
-    dfs(i+1, res + data[i])
-
-    dfs(i+1, res)
-    
-
-
 n, k = map(int, input().split())
-
 data = list(map(int, input().split()))
+d = dict()
+
 ans = 0
-dfs(0,0)
+for elem in data:
+    diff  = k - elem
+
+    if diff in d:
+        ans += d[diff]
+    
+    if elem in d:
+        d[elem] += 1
+    else:
+        d[elem] = 1
+
+
 print(ans)
