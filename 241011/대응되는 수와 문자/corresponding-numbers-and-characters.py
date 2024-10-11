773309cbaf0ac562cae6426818c2cd888d0498ce
JUNSEO PARK <pjs9177@naver.com>
2024-10-11 16:45:02 +0900

    Title: 대응되는 수와 문자, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241011/대응되는 수와 문자/corresponding-numbers-and-characters.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+
+d = dict()
+
+for i in range(n):
+    com = str(input())
+    d[i + 1] = com
+
+    
+
+
+for _ in range(m):
+    com = input()
+    if com.isdigit():
+        com = int(com)
+        print(d[com])
+    else:
+        for j in range(1,len(d)):
+            if d[j] == com:
+                print(j)
