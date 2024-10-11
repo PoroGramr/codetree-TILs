261f784326b6d1715159f98fca6c95a9277f688f
JUNSEO PARK <pjs9177@naver.com>
2024-10-11 16:18:03 +0900

    Title: 숫자 등장 횟수, Time: 226ms, Memory: 34MB, Status: Passed - CodeTree

--- 241011/숫자 등장 횟수/number-frequency.py
+++ 241011/숫자 등장 횟수/number-frequency.py
@@ -1,13 +1,21 @@
-n, m = map(int, input().split())
+n, m = map(int, input().split()) # 원소의 개수 n, 질의의 수 m
 
-d = dict()
-data= list(map(int, input().split()))
-com = list(map(int, input().split()))
+
+data= list(map(int, input().split())) # 입력 받은 데이터 리스트
+
+com = list(map(int, input().split())) # 입력 받은 검색해야하는 데이터 리스트
+
+d = dict()  # 딕셔너리 생성
+
 for i in range(n):
+    # 만약 딕셔너리에 해당 데이터가 없다면
     if not data[i] in d:
         d[data[i]] = 1
+        
+    # 있다면 +1
     else:
         d[data[i]] += 1
+
 for j in range(m):
     if com[j] in d:
         print(d[com[j]], end = " ")
