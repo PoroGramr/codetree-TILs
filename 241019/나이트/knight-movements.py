00364e671d7b14a42444c5aa550b5f2e03c74335
JUNSEO PARK <pjs9177@naver.com>
2024-10-19 19:17:42 +0900

    Title: 나이트, Time: 135ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241019/나이트/knight-movements.py
+++ 241019/나이트/knight-movements.py
@@ -4,20 +4,22 @@ from collections import deque
 def bfs(y,x):
     q = deque()
     q.append((y,x))
+    rt = 9999
     count = 0
 
     while q:
         cy, cx = q.popleft()
 
         if cy == r2 - 1 and cx == c2 - 1:
-            return count
+            rt = min(count, rt)
         
         for py, px in ((-1, -2),(-2,-1),(-2,1),(-1,2),(1,-2),(2,-1),(2,1),(1,2)):
             ny, nx = cy + py, cx + py
             if 0 <= ny < n and 0 <= nx < n and (data[ny][nx] == 0 or data[ny][nx] == 1) :
                 q.append((ny,nx))
+                count += 1
 
-    return -1
+    return rt
 
 n = int(input())
 
