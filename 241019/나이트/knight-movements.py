90a36ab1210971aec93e40b96a66eae455e8c649
JUNSEO PARK <pjs9177@naver.com>
2024-10-19 19:13:05 +0900

    Title: 나이트, Time: 2295ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 241019/나이트/knight-movements.py
+++ 241019/나이트/knight-movements.py
@@ -6,12 +6,12 @@ def bfs(y,x):
     q.append((y,x))
     count = 0
     while q:
-        if y == r2 -1 and x == c2 - 1:
-            return count
         cy, cx = q.popleft()
+        if cy == r2 -1 and cx == c2 - 1:
+            return count
         for py, px in ((-1, -2),(-2,-1),(-2,1),(-1,2),(1,-2),(2,-1),(2,1),(1,2)):
             ny, nx = cy + py, cx + py
-            if 0 <= ny < n and 0 <= nx < n and data[ny][nx] == 0:
+            if 0 <= ny < n and 0 <= nx < n and (data[ny][nx] == 0 or data[ny][nx] == 1) :
                 q.append((ny,nx))
 
 
