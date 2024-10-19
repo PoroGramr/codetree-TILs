84fc690bdfd9e4c9911fe40ac176d9919a0d94f3
JUNSEO PARK <pjs9177@naver.com>
2024-10-19 19:24:15 +0900

    Title: 나이트, Time: 160ms, Memory: 25MB, Status: Passed - CodeTree

--- 241019/나이트/knight-movements.py
+++ 241019/나이트/knight-movements.py
@@ -1,30 +1,34 @@
 from collections import deque
 
-
-def bfs(y,x):
+def bfs(start_y, start_x):
     q = deque()
-    q.append((y,x))
+    q.append((start_y, start_x))
 
     while q:
         cy, cx = q.popleft()
 
+        # 도착점에 도달하면 현재 이동 횟수를 반환
         if cy == r2 - 1 and cx == c2 - 1:
             return data[cy][cx]
         
-        for py, px in ((-1, -2),(-2,-1),(-2,1),(-1,2),(1,-2),(2,-1),(2,1),(1,2)):
-            ny, nx = cy + py, cx + py
-            if 0 <= ny < n and 0 <= nx < n and (data[ny][nx] == 0 or data[ny][nx] == 1) :
-                q.append((ny,nx))
+        # 나이트의 8가지 이동 방향
+        for dy, dx in ((-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1)):
+            ny, nx = cy + dy, cx + dx
+            
+            # 유효한 범위인지 확인하고, 방문하지 않았던 위치인지 확인
+            if 0 <= ny < n and 0 <= nx < n and data[ny][nx] == -1:
+                # 거리 업데이트
                 data[ny][nx] = data[cy][cx] + 1
+                q.append((ny, nx))
 
-    return -1
+    return -1  # 도달할 수 없는 경우
 
 n = int(input())
 
 r1, c1, r2, c2 = map(int, input().split())
 data = [[-1] * n for _ in range(n)]
-data[r2 - 1][c2 - 1] = 0
+data[r1 - 1][c1 - 1] = 0  # 시작 위치에서의 거리 초기화
 
-ans = bfs(r1-1,c1-1)
+ans = bfs(r1 - 1, c1 - 1)
 
 print(ans)
