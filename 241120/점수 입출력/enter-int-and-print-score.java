f94eb951681490b6923efcd5637d4bb1715a4e77
JUNSEO PARK <pjs9177@naver.com>
2024-11-20 14:09:35 +0900

    Title: 점수 입출력, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241120/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+
+        System.out.print("Your score is ");
+        System.out.print(a);
+        System.out.print(" point.");
+    }
+}
