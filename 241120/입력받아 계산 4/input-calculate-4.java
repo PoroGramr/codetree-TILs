18eef20f3dee08224bef20d0f245fddafbcb12ff
JUNSEO PARK <pjs9177@naver.com>
2024-11-20 14:10:54 +0900

    Title: 입력받아 계산 4, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241120/입력받아 계산 4/input-calculate-4.java
@@ -0,0 +1,10 @@
+import java.util.scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        a = s.nextInt():
+
+        System.out.println("a * 2");
+    }
+}
