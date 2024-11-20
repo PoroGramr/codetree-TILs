f494e872ba32b06dc0dc8dc9abaa8b0156547f26
JUNSEO PARK <pjs9177@naver.com>
2024-11-20 14:12:35 +0900

    Title: 입력받아 계산 4, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 241120/입력받아 계산 4/input-calculate-4.java
+++ 241120/입력받아 계산 4/input-calculate-4.java
@@ -1,11 +1,11 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        a = s.nextInt();
+        int a = s.nextInt();
 
-        System.out.println("a * 2");
+        System.out.println(a * 2);
     }
 }
