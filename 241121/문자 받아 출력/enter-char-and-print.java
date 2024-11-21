a343ef32843f23afd55b7af4e6488f47bd00950e
JUNSEO PARK <pjs9177@naver.com>
2024-11-21 12:45:02 +0900

    Title: 문자 받아 출력, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241121/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String c = s.next();
+        System.out.println(c);
+
+    }
+}
