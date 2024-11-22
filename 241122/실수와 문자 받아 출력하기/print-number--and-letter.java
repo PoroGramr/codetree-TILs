56e8c7178117dd487bb2fa5e7610813f46947879
JUNSEO PARK <pjs9177@naver.com>
2024-11-22 15:50:23 +0900

    Title: 실수와 문자 받아 출력하기, Time: 152ms, Memory: 11MB, Status: Passed - CodeTree

--- 241122/실수와 문자 받아 출력하기/print-number--and-letter.java
+++ 241122/실수와 문자 받아 출력하기/print-number--and-letter.java
@@ -12,7 +12,7 @@ public class Main {
 
         System.out.println(a);
         b = Math.round(b * 100) / 100.0;
-        System.out.printf("%.2f", b);
+        System.out.printf("%.2f\n", b);
         c = Math.round(c * 100) / 100.0;
         System.out.printf("%.2f", c);
 
