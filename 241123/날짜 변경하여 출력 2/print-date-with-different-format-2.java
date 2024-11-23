d44189340139447445fd9d170a333f753cf16572
JUNSEO PARK <pjs9177@naver.com>
2024-11-23 19:41:07 +0900

    Title: 날짜 변경하여 출력 2, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241123/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        s.useDelimiter("-");
+
+        int m = s.nextInt();
+
+        int d = s.nextInt();
+
+        int y = s.nextInt();
+
+        System.out.printf("%d.%d.%d", y,m,d);
+    }
+}
