1b82037192c27771f471f02b70fd524321885eb2
JUNSEO PARK <pjs9177@naver.com>
2025-03-26 21:44:51 +0900

    Title: 증가시키며 출력하기, Time: 92ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250326/증가시키며 출력하기/increase-and-print.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+
+        for(int i  =5; i <= 17 ; i = i + 2){
+            System.out.print(i + " ");
+        }
+    }
+}
