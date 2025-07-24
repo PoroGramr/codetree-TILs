1b5d444c28684d8b98f99840be61f07c790da9b3
JUNSEO PARK <pjs9177@naver.com>
2025-07-24 15:41:42 +0900

    Title: 갈 수 있는 곳들, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250724/갈 수 있는 곳들/places-can-go.py
@@ -0,0 +1,5 @@
+n, k = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+points = [tuple(map(int, input().split())) for _ in range(k)]
+
+# Please write your code here.
