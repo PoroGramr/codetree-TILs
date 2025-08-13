555fc3591d3733f445fba5ee99c15995b73c9bf9
JUNSEO PARK <pjs9177@naver.com>
2025-08-13 16:41:30 +0900

    Title: 선분 위의 점, Time: 493ms, Memory: 27MB, Status: Passed - Codetree

--- 250813/선분 위의 점/point-on-the-line-segment.py
+++ 250813/선분 위의 점/point-on-the-line-segment.py
@@ -1,4 +1,34 @@
-import bisect
+def lower(arr, target):
+    left = 0
+    right = len(arr) - 1
+    idx = len(arr)
+
+    while left <= right:
+        mid = (left+right) // 2
+
+        if arr[mid] >= target:
+            idx = min(idx, mid)
+            right = mid - 1
+        else:
+            left = mid + 1      
+    return idx  
+
+def upper(arr, target):
+    left = 0
+    right = len(arr) - 1
+    idx = -1
+
+    while left <= right:
+        mid = (left+right) // 2
+
+        if arr[mid] <= target:
+            idx = max(idx, mid)
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return idx
+
 
 N, M = map(int, input().split())
 points = list(map(int, input().split()))
@@ -6,6 +36,6 @@ points.sort()
 
 for _ in range(M):
     s, e = map(int, input().split())
-    left = bisect.bisect_left(points, s)   
-    right = bisect.bisect_right(points, e) 
-    print(right - left)
+    a = lower(points,s)
+    b = upper(points,e)
+    print(b-a + 1)
