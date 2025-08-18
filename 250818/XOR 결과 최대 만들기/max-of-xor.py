43777315994187d8322274fff8a7774c8f16c898
JUNSEO PARK <pjs9177@naver.com>
2025-08-18 20:46:40 +0900

    Title: XOR 결과 최대 만들기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250818/XOR 결과 최대 만들기/max-of-xor.py
@@ -0,0 +1,29 @@
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+
+# Please write your code here.
+
+answer = 0
+
+
+arr = []
+def dfs(curNum,start):
+    global answer 
+    if start == m + 1:
+
+        res = 0
+        for a in arr:
+            res ^= a
+
+        answer = max(res,answer)
+        return
+
+
+    for i in range(start, n + 1):
+        arr.append(i)
+        dfs(curNum + 1, start + 1)
+        arr.pop()
+
+
+dfs(1, 1)
+print(answer)
