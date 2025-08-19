49cc3a548ba9a6673b5cb798d42116d88572bbde
JUNSEO PARK <pjs9177@naver.com>
2025-08-19 10:28:14 +0900

    Title: 2N개 중에 N개의 숫자를 적절하게 고르기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250819/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
@@ -0,0 +1,28 @@
+n = int(input())
+num = list(map(int, input().split()))
+
+# Please write your code here.
+arr1 =[]
+arr2 = []
+
+answer = sum(num)
+numSum = sum(num)
+def choose(curNum,start):
+    global answer, numSum
+    if curNum == n + 1:
+        tmp = sum(arr1)
+        rest = numSum - tmp
+
+        bet = abs(tmp - rest)
+        answer = min(answer, bet)
+        return
+    
+    for i in range(start,2*n):
+        arr1.append(num[i])
+        choose(curNum + 1, start + 1)
+        arr1.pop()
+
+
+choose(1,0)
+
+print(answer)
