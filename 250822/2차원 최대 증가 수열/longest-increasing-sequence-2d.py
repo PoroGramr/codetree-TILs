f97cc6cb0558009306314c4a1243c73525f8a1ff
JUNSEO PARK <pjs9177@naver.com>
2025-08-22 16:21:40 +0900

    Title: 2차원 최대 증가 수열, Time: 74ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250822/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+dp = [[0] * n for _ in range(m)]
+dp[0][0] = 1
+for i in range(n):
+    for j in range(m):
+        for a in range(i + 1,n):
+            for b in range(j + 1,m):
+                # if (a== 0 and b == 0) or (a == i and b == j):
+                #     continue
+                # print("i",i,"j",j,"a",a,"b",b)
+                if grid[a][b] > grid[i][j]:
+                    dp[a][b] = max(dp[a][b] ,dp[i][j] + 1)
+
+answer = 0
+for y in range(n):
+    for x in range(m):
+        answer = max(dp[y][x], answer)
+
+print(answer)
